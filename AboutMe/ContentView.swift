c940414c750381127c13b81c6141867c6b7f0df1
Willa Mayrose <francesmay775@gmail.com>
2025-06-04 13:22:47 -0500

    Test push command

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -1,23 +1,41 @@
 //
 //  ContentView.swift
-//  AboutMe
+//  STEMStars
 //
-//  Created by Scholar on 6/4/25.
+//  Created by Scholar on 6/3/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(.teal)
+                .ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("JoyBuolamwini")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                HStack(spacing: 20){
+                    Text("Willa Mayrose")
+                        .font(.title)
+                        .fontWeight(.bold)
+            Text("Poet of Code")
+                }
+            Text("Founder of the Algorithmic Justice League. Uses art and research to illuminate social implications of AI and to promote equitable and accountable technology.")
+            } .padding()
+            .background(Rectangle().foregroundColor(.white))
+            .cornerRadius(15)
+            .shadow(radius: 15)
+            .padding()
+// VStack end
+        } // ZStack end
+       
+        
+        
         }
-        .padding()
     }
-}
 
 #Preview {
     ContentView()
