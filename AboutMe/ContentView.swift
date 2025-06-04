60cc0a97e8594dd2f16f5b116ddfce73316a40d5
Willa Mayrose <francesmay775@gmail.com>
2025-06-04 13:56:05 -0500

    image added

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            Color(.teal)
+            Color("midnightGreen")
                 .ignoresSafeArea()
             VStack(alignment: .leading, spacing: 20.0) {
-                Image("JoyBuolamwini")
+                Image("me")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .cornerRadius(15)
@@ -21,12 +21,16 @@ struct ContentView: View {
                     Text("Willa Mayrose")
                         .font(.title)
                         .fontWeight(.bold)
+                        .foregroundColor(.white)
             Text("Poet of Code")
+                        .foregroundColor(.white)
                 }
-            Text("Founder of the Algorithmic Justice League. Uses art and research to illuminate social implications of AI and to promote equitable and accountable technology.")
+                Text("Founder of the Algorithmic Justice League. Uses art and research to illuminate social implications of AI and to promote equitable and accountable technology.")
+                    .foregroundColor(.white)
             } .padding()
-            .background(Rectangle().foregroundColor(.white))
+            .background(Rectangle().foregroundColor(.black))
             .cornerRadius(15)
+            .frame(height: 900.0)
             .shadow(radius: 15)
             .padding()
 // VStack end
