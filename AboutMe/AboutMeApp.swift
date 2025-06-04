b3fa86bd0d2a4392c476aac9b530090f890af032
Willa Mayrose <francesmay775@gmail.com>
2025-06-04 13:18:59 -0500

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Scholar on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
